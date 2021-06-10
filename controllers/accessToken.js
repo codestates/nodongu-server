14f21878228083922b36eedab11c60ca60ed723a
Park Sangwoo <88parksw@gmail.com>
2021-06-10 17:07:16 +0900

    Signup

--- controllers/accessToken.js
+++ controllers/accessToken.js
@@ -4,21 +4,21 @@ require("dotenv").config();
 module.exports = {
   makeAccessToken: (data) => {
     return sign(data, process.env.ACCESS_SALT, { expiresIn: "1h" });
-  },
+  }, 
   makeRefreshToken: (data) => {
     return sign(data, process.env.REFRESH_SALT, { expiresIn: "7d" });
   },
   resRefreshToken: (res, refreshToken) => {
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true
-    });
+    }); // refreshToken 보내기
   },
   resAccessToken: (res, accessToken) => {
     res.json({ data: { accessToken }, message: "ok" });
-  },
+  }, // accessToken 보내기
   reissueAccessToken: (res, accessToken, data) => {
     res.json({ data: { accessToken, userInfo: data }, message: "ok" });
-  },
+  }, // 
   isAuthorized: (req) => {
     const authorization = req.headers["authorization"];
     if (!authorization) {
@@ -29,7 +29,7 @@ module.exports = {
       return verify(token, process.env.ACCESS_SALT);
     } catch (err) {
       return null;
-    }
+    } // 요청이 있을때마다 토큰 검증.
   },
   authRefeshToken: (refreshToken) => {
     try {
@@ -37,5 +37,5 @@ module.exports = {
     } catch (err) {
       return null;
     }
-  },
+  }, // refresh 토큰 유효성 확인.
 };
