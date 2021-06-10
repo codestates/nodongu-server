8f8bc7b55f5ab876645f8784ea4f311121f58127
Park Sangwoo <88parksw@gmail.com>
2021-06-10 13:51:17 +0900

    add login signup

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,84 @@
-// const axios = require('axios'); 
-// CRUD routing 
+// 회원 CRUD routing
+const express = require('express');
+const bodyParser = require('body-parser');
 
-// youtube api도 여기에 할지 따로 컨트롤러로 뺄지 고민. 
+// const {users} = require('../models'); // db에서 users table 땡겨오기.
+
+// const {
+//     generateAccessToken,
+//     generateRefreshToken,
+//     sendRefreshToken,
+//     sendAccessToken,
+//   } = require('../tokenFunctions');
+
+module.exports = {
+    signIn: async (req, res) => {
+       const {email, password} = req.body;
+       
+       await users.findOne({
+           where: {
+               email,
+               password
+           },
+       }).then(userData => {
+
+       // 로그인 정보와 일치하는 유저의 정보의 형태는 {email: , nickname: , password: , image: , token: , created_at:, updated_at: ,id:,  }
+       
+            if(!userData) {
+                res.status(404).send('Invalid user') //data 형식을 보내줄 것       
+            } 
+            
+            delete userData.dataValues.password // 패스워드는 삭제시켜주자. 
+            // const accessToken = generateAccessToken(data.dataValues); // 토큰은 다른 모듈에서 생성되서 전달될 예정. 
+            // const refreshToken = generateRefreshToken(data.dataValues);
+
+            res.status(200).json(userData) // accessToken, userData 삽입 및 전송
+            // 일단 crud만 구현해보자!
+            
+        }).catch(err => {
+            console.log(err);
+        })
+    },
+
+    signOut: async (req, res) => {
+
+
+
+
+    },
+
+
+    signUp: async (req, res) => { //header에 authorization, body에 nickname, password, email 
+        const {nickname, email, password} = req.body;
+        
+        await users.findOne({
+            where: {email}
+        }).then(existedData => {
+            
+            if (existedData) { // db에 email이 이미 존재할 경우, 400 status와 에러메세지 뿜뿜
+                res.status(409).send("Is the email that already exists.")
+            } 
+             // db에 회원정보가 없으니 정상적으로 회원가입 진행/ 
+            await users.create({
+                email: req.body.email,
+                nickname: req.body.nickname,
+                password: req.body.password, // responseData에 id, createdAt, updatedAt 포함되는지 확인. 
+                }).then(responseData => res.status(201).send(responseData))
+                
+        }).catch(err => {
+            console.log(err);
+        })
+    },
+    
+    userInfo: async (req, res) => {
+
+
+    }, 
+    
+    modifyInfo: async (req, res) => {
+
+
+    }
+
+
+}
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const cors = require("cors");
 const port = 80;
 const cors = require('cors');
 const logger = require('morgan');
@@ -9,13 +8,19 @@ const cookieParser = require("cookie-parser");
 
 // const userControllers = require("./controllers/index.js"); // script 작성 후 주석해제 요청
 // const myListControllers = require("./controllers/myList.js");
-
-app.use(cors({
+// 쿠키파서 제이슨, 익스프레스 제이슨
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
   origin: "*",
   methods: ["GET, POST, OPTIONS"],
   credentials: true,
   preflightContinue: true
-}));
+})
+);
+
+
 
 function handleListening(req, res) {
   res.send('hello world')
