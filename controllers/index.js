ceb905b81a9246fa6c458362d6d9b714f15ae4af
Park Sangwoo <88parksw@gmail.com>
2021-06-11 13:12:47 +0900

    add modifyInfo

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 // 회원 CRUD routing
 const express = require('express');
 const bodyParser = require('body-parser');
-// const {users} = require('../models'); // db에서 users table 땡겨오기.
+const {user} = require('../models/user'); // db에서 users table 땡겨오기.
 const {
     makeAccessToken,
     makeRefreshToken,
@@ -23,7 +23,7 @@ module.exports = {
         } // routing 시험용 코드
        
        // 이메일이 존재하지 않을 때, 비밀번호가 틀렸을 때 각각 다른 404 res 보내줄 것. 
-       await users.findOne({
+       await user.findOne({
            where: {
                email: req.body.email // e-mail 여부 확인
            }
@@ -31,7 +31,7 @@ module.exports = {
             if(!data) {
                 res.status(404).send({loginSuccess: false, message: "존재하지 않는 이메일입니다"}) 
             } else {
-                users.findOne({
+                user.findOne({
                     where: {
                         password: req.body.password // password 일치여부 확인 
                     }
@@ -76,18 +76,17 @@ module.exports = {
         // } else {
         //     res.status(404).send("Not enough informations!")
         // } // routing 시험용 코드
+        // const {nickname, email, password} = req.body;
         
-        const {nickname, email, password} = req.body;
-        
-        await users.findOne({
-            where: {email}
+        await user.findOne({
+            where: {email: req.body.email}
         }).then(existedData => {
             
             if (existedData) { // db에 email이 이미 존재할 경우, 400 status와 에러메세지 뿜뿜
                 res.status(409).send("Is the email that already exists.")
             } 
              // db에 회원정보가 없으니 정상적으로 회원가입 진행/ 
-            users.create({
+            user.create({
                 email: req.body.email,
                 nickname: req.body.nickname,
                 password: req.body.password, // responseData에 id, createdAt, updatedAt 포함되는지 확인. 
@@ -100,10 +99,6 @@ module.exports = {
     
     getUserInfo: async (req, res) => {
 
-        // JWT 내 payload에서 사용자 정보를 갖고 있어서 확인작업 필요. 
-
-        res.send("getUserInfo 구현 중입니다!")
-
         const accessTokenData = isAuthorized(req); // JWT 토큰 해독
         if (!accessTokenData) {
            res.send("토큰이 유효하지 않습니다!"); 
@@ -111,7 +106,7 @@ module.exports = {
 
         const {email} = accessTokenData;
         
-        await users.findOne({
+        await user.findOne({
             where: email
         }).then(data => {
             
@@ -125,13 +120,47 @@ module.exports = {
             console.log(err);
         })
 
-        res.status(500).send("");
+        res.status(500).send("error");
     }, 
     
     modifyInfo: async (req, res) => {
 
-        console.log("modifyInfo routing 구현 중입니다")
-    }
+        // 로그인 상태인지 확인
+        // 바디로 nickname, image, password 확인
+        // db에 해당하는 유저 정보 찾은 뒤 (메일주소)
+        // 입력된 정보를 수정한다. sequelize에 수정기능.
 
+        const accessTokenData = isAuthorized(req); // 토큰 해독
+        const {email} = accessTokenData; // 유저정보 확인    
+        // if (!accessTokenData) {
+        //    res.send("토큰이 유효하지 않습니다!"); 
+        // }
+
+        // 닉네임 일치여부 확인
+        const checkNickname = await user.findOne({
+            where: {
+                nickname: req.body.nickname
+            }
+        }).then(data => {
+            
+            if(data) {
+                res.status(409).send("Is the nickname that already exists.")
+            } else {
+                await user.update(
+                    {nickname: req.body.nickname},
+                    {image: req.body.image},
+                    {password: req.body.password},
+                    {where: email}        
+                ).then(data => {
+                    res.status(200).send(data)
+                })
+            }
+        }).catch(err => {
+            console.log(err);
+        })
+
+        res.status(500).send("error");
+        
+    }
 
 }
