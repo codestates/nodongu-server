39aa879672482d91d92c7f44e53acb4cc4162467
skyupguf <skyupguf@gmail.com>
2021-06-14 14:40:09 +0900

    Add query for thumbnail

--- controllers/myList.js
+++ controllers/myList.js
@@ -1,38 +1,53 @@
 const { user, mylist, play, playlist } = require("../models")
 
 module.exports = {
-  /*  Client에 userId 추가 요청  */
-  addMyList: async(req, res) => {
-    const { userId, title } = req.body
-    const find = await mylist.findOne({
-      where: { userId: userId },
-      attributes: ['id']
-    });      
+  
+    addMyList: async(req, res) => {
 
-    if(find === null) {
-      res.status(404).send({ success: false })
-    } else {
-      mylist.create({
-        listTitle: title, userId: userId
-      }).then(make => {
-        res.status(200).send({ success: true }) 
-      }).catch(err => res.send(err))
-    }
-  },
+        const { userId, title } = req.body     
+        const find = await mylist.findOne({
+            where: { userId: userId },
+            attributes: ['id']
+        });      
+
+        if(find === null) {
+            res.status(404).send({ success: false })
+        
+        } else {
+            mylist.create({
+                listTitle: title, userId: userId
+            }).then(make => {
+                res.status(200).send({ success: true }) 
+            }).catch(err => res.send(err))
+        }
+    },
   
   
-  getMyList: async (req, res) => {
-    const find = await mylist.findAll({
-      where: { userId: req.body.userId },
-      attributes: ['id', 'listTitle', 'createdAt']
-    });
-      
-    if(find.length === 0) {
-      res.status(404).send({ success: false })
-    } else {
-      let data = find.map(el => el.dataValues)
-      res.status(200).send({ success: true, data: data })
-    }
-  }
+    getMyList: async (req, res) => {
 
+        const find = await mylist.findAll({
+            where: { userId: req.body.userId },
+            attributes: ['id', 'listTitle', 'createdAt'],
+            include: [{
+                model: play,
+                attributes: ['thumbnail'],
+                through: { attributes: ['mylistId'] }
+            }]
+        });
+
+        if(find.length === 0) {
+            res.status(404).send({ success: false })
+
+        } else {
+            let data = [];
+            find.forEach(el => {
+                let obj = el.dataValues;
+                obj.thumbnails = el.dataValues.plays.map(el => el.thumbnail);
+                delete obj.plays;
+                data.push(obj);
+            });
+            res.status(200).send({ success: true, data: data })
+        }
+    }
 }
+ 
