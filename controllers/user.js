20a128d2a4bb35d0616b69b551891fcb72760414
SashainSPb <74516322+SashainSPb@users.noreply.github.com>
2021-06-16 17:54:40 +0900

    fix youtubeAPI

--- controllers/user.js
+++ controllers/user.js
@@ -59,7 +59,7 @@ module.exports = {
     },
 
     logOut: (req, res) => {
-        
+  
         const refreshTokenData = isAuthorized(req); // 토큰 유효성 검증
         console.log("refreshTokenData: ", refreshTokenData) 
 
@@ -138,88 +138,6 @@ module.exports = {
 
     }, 
 
-    modNickname: async (req, res) => {
-
-        await user.findOne({
-            where: {
-                id: req.body.userId
-            }
-        })
-        .then(data1 => {
-
-            if(!data1) {
-                return res.status(202).send({success: false}) // status 204 출력이 안되는 오류. 
-            }
-
-            user.update({
-                nickname: req.body.nickname
-            },
-            {
-                where: {id: req.body.userId}
-            })
-            .then(data2 => res.status(200).send({success: true}) )
-
-        }).catch(err => {
-            res.status(500).send(err);
-        })  
-
-    },
-
-    modImage: async (req, res) => { // Image data handling은 대기.
-
-        await user.findOne({
-            where: {
-                id: req.body.userId
-            }
-        })
-        .then(data1 => {
-
-            if(!data1) {
-                return res.status(202).send({success: false})
-            }
-
-            user.update({
-                image: req.body.image
-            },
-            {
-                where: {id: req.body.userId}
-            })
-            .then(data2 => res.status(200).send({success: true}) )
-
-        }).catch(err => {
-            res.status(500).send(err);
-        }) 
-
-    },
-    
-    modPassword: async (req, res) => {
-
-        await user.findOne({
-            where: {
-                id: req.body.userId
-            }
-        })
-        .then(data1 => {
-
-            if(!data1) {
-                return res.status(202).send({success: false}) 
-            }
-
-            user.update({
-                password: req.body.password
-            },
-            {
-                where: {id: req.body.userId}
-            })
-            .then(data2 => res.status(200).send({success: true}) )
-
-        }).catch(err => {
-            res.status(500).send(err);
-        }) 
-
-    },
-
-     
     deleteUser: async (req,res) => {
 
         await user.findOne({ // userId와 일치하는 레코드가 있는지 확인.
