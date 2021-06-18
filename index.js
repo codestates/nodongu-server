664f6a4858a2f854c3603b374a34262072124cc1
skyupguf <75276357+skyupguf@users.noreply.github.com>
2021-06-18 12:24:40 +0900

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false })); 
 app.use(cookieParser())
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "http://nodongu-client-quokka.s3-website.ap-northeast-2.amazonaws.com",
   methods: ["GET", "POST", "OPTIONS"],
   credentials: true,
   preflightContinue: true
